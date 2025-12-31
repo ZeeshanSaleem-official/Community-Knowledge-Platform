d4135355c0b5e4c5879484e37e0fc33489f8a300
Zeeshan Saleem <l1f22bsse0031@ucp.edu.pk>
2025-12-31 19:37:25 +0500

    Fixes minor bugs in  new login and sign up fucntionalities

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,10 +1,13 @@
 import LoginForm from "@/components/auth/login-form";
+import { Suspense } from "react";
 
 export default function SignInPage() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md">
-        <LoginForm />
+        <Suspense>
+          <LoginForm />
+        </Suspense>
       </div>
     </div>
   );
